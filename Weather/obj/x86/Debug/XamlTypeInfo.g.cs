e3ae9699f284ec64c3d54afb4a3aacbecf7e0b1f
Balance37 <j546wang@outlook.com>
2016-05-24 23:54:49 -0400

    Added map and event handler

--- Weather/obj/x86/Debug/XamlTypeInfo.g.cs
+++ Weather/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -132,15 +132,17 @@ namespace Weather.Weather_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[3];
+            _typeNameTable = new string[4];
             _typeNameTable[0] = "Weather.MainPage";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
+            _typeNameTable[3] = "Weather.Map.Map";
 
-            _typeTable = new global::System.Type[3];
+            _typeTable = new global::System.Type[4];
             _typeTable[0] = typeof(global::Weather.MainPage);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
+            _typeTable[3] = typeof(global::Weather.Map.Map);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -176,6 +178,7 @@ namespace Weather.Weather_XamlTypeInfo
         }
 
         private object Activate_0_MainPage() { return new global::Weather.MainPage(); }
+        private object Activate_3_Map() { return new global::Weather.Map.Map(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -200,6 +203,13 @@ namespace Weather.Weather_XamlTypeInfo
 
             case 2:   //  Windows.UI.Xaml.Controls.UserControl
                 xamlType = new global::Weather.Weather_XamlTypeInfo.XamlSystemBaseType(typeName, type);
+                break;
+
+            case 3:   //  Weather.Map.Map
+                userType = new global::Weather.Weather_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_3_Map;
+                userType.SetIsLocalType();
+                xamlType = userType;
                 break;
             }
             return xamlType;
